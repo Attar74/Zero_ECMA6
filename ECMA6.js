199137c574b28ac34a280bbb30fd027295cf85e6
mahmod <mahmoudelattar800@gmail.com>
2020-05-29 00:58:00 +0200

    const & let VS var

---
+++ ECMA6.js
@@ -0,0 +1,24 @@
+
+/****************************Lesson one to three**********************/
+/*
+let
+-Block Scope
+-Accessing vars before decleration will return refrence error
+- Doesn't create properties in the window Object
+,
+const
+-Block Scope
+-Cann't be redeclerared but can be reassign
+-Accessing vars before decleration will return refrence error
+- Doesn't create properties in the window Object
+
+VS
+
+var
+-Function Scope
+-Can be redeclerared
+-Accessing vars before decleration will return undefined
+- create properties in the window Object
+*/
+
+/****************************Lesson four to three**********************/
