c5ed6ad0bd270489d2fd4d3b93a169344b929ee2
mahmod <mahmoudelattar800@gmail.com>
2020-05-29 17:30:12 +0200

    lesson 6

--- ECMA6.js
+++ ECMA6.js
@@ -21,4 +21,26 @@ var
 - create properties in the window Object
 */
 
-/****************************Lesson four to three**********************/
+/****************************Lesson four & five**********************/
+/*
+  Arrow Function
+
+//with no parameters
+let ar1Function = _ => 2;
+
+//with one parameters
+let ar2Function = x => x;
+
+//with more than one parameters
+let ar3Function = (x,y) => x*y;
+
+
+//this with arrow function is referring to the function scope
+*/
+
+/****************************Lesson six**********************/
+/*
+Template literals  `   `
+using it is making things more eaiser
+
+*/
